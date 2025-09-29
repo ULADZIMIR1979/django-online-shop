2ce5264c097bccb5b3c2a3da68f9026bc044f36f
Владимир Лешанков <vovan1979.79@mail.ru>
2025-09-29 23:43:57 +0300

    add homework-6

--- mysite/requestdataapp/views.py
+++ mysite/requestdataapp/views.py
@@ -1,8 +1,8 @@
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.conf import settings
-import os
+
+from .forms import UserBioForm, UploadFileForm
 
 
 def process_get_view(request: HttpRequest) -> HttpResponse:
@@ -18,25 +18,37 @@ def process_get_view(request: HttpRequest) -> HttpResponse:
 
 
 def user_form(request: HttpRequest) -> HttpResponse:
-    return render(request, 'requestdataapp/user-bio-form.html')
+    context = {
+        "form": UserBioForm(),
+    }
+    return render(request, 'requestdataapp/user-bio-form.html', context=context)
 
 
 def handle_file_upload(request: HttpRequest) -> HttpResponse:
-    context = {}
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            myfile = form.cleaned_data['file']
 
-    if request.method == 'POST' and request.FILES.get('myfile'):
-        myfile = request.FILES['myfile']
+            # Проверка размера файла (1 МБ = 1048576 байт)
+            MAX_FILE_SIZE = 1048576  # 1 МБ
 
-        # Проверка размера файла (1 МБ = 1048576 байт)
-        MAX_FILE_SIZE = 1048576  # 1 МБ
+            if myfile.size > MAX_FILE_SIZE:
+                error_message = f'Файл слишком большой. Максимальный размер: {MAX_FILE_SIZE // 1048576} МБ'
+                return render(request, 'requestdataapp/file-upload.html', {
+                    'form': form,
+                    'error': error_message
+                })
+            else:
+                fs = FileSystemStorage()
+                filename = fs.save(myfile.name, myfile)
+                print("saved file", filename)
+                return render(request, 'requestdataapp/file-upload.html', {
+                    'form': UploadFileForm(),
+                    'success': f'Файл {filename} успешно загружен',
+                    'file_size': myfile.size
+                })
+    else:
+        form = UploadFileForm()
 
-        if myfile.size > MAX_FILE_SIZE:
-            context['error'] = f'Файл слишком большой. Максимальный размер: {MAX_FILE_SIZE // 1048576} МБ'
-        else:
-            fs = FileSystemStorage()
-            filename = fs.save(myfile.name, myfile)
-            print("saved file", filename)
-            context['success'] = f'Файл {filename} успешно загружен'
-            context['file_size'] = myfile.size
-
-    return render(request, 'requestdataapp/file-upload.html', context=context)
+    return render(request, 'requestdataapp/file-upload.html', {'form': form})
---
+++ mysite/shopapp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.core import validators
+
+from .models import Product
+
+
+# class ProductForm(forms.Form):
+#     name = forms.CharField(max_length=100)
+#     price = forms.DecimalField(min_value=1, max_value=100000, decimal_places=2)
+#     description = forms.CharField(
+#         label="Product description",
+#         widget=forms.Textarea(attrs={"rows": 5, "cols": 30}),
+#         validators=[validators.RegexValidator(
+#             regex=r"great",
+#             message="Description must contain the word 'great'",
+#         )],
+#     )
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "name", "price", "description", "discount"
